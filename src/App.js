8917bfff8781a13b00ccf03d65e2e9fab753dbec
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-11-07 21:50:18 +0700

    add: router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Map from './map/Map';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import Form from './Form';
 
 function App() {
   return (
-    <Map />
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route path="/form">
+            <Form />
+          </Route>
+
+          <Route path="/map">
+            <Map />
+          </Route>
+
+        </Switch>
+      </BrowserRouter>
+
+    </>
   );
 }
 
